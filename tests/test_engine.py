709947ac7f4e55e8fc6bf7099ba89c9e4e627242
AtakanTekparmak <atakantekerparmak@gmail.com>
2024-10-23 16:02:17 +0200

    fix: Removed failing outdated test

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -6,12 +6,3 @@ def test_execute_python_code_basic():
     output = execute_python_code(code)
     assert output['variables'] == {'x': 5, 'y': 10, 'result': 15}
     assert output['function_results'] == {}
-
-def test_execute_python_code_with_function():
-    def multiply(a, b):
-        return a * b
-
-    code = "result1 = multiply(3, 4)\nresult2 = multiply(5, 6)"
-    output = execute_python_code(code, functions=[multiply])
-    assert output['variables'] == {'result1': 12, 'result2': 30}
-    assert output['function_results'] == {'multiply': [12, 30]}
